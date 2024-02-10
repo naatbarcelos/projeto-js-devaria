06d274f0b5c0dba67f8335e142e0e73d2e501f0b
naatbarcelos <naathalia.b@hotmail.com>
2024-02-09 23:04:18 -0300

    Validação do formulário

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,48 @@
-const $stepText = $('step.text');
+const $stepText = $('#step-text');
+const $stepDescription = $('#step-description');
 const $stepOne = $('.step.one');
 const $stepTwo = $('.step.two');
 const $stepThree = $('.step.three');
 
+const $inputNome = $('#nome');
+const $inputSobrenome = $('#sobrenome');
+const $inputDataNascimento = $('#dataNascimento');
+
 function init(){
+    $stepText.text('Passo 1 de 3 - Dados pessoais');
+    $stepDescription.text('Descreva seus dados para que possamos te conhecer melhor.');
     $stepTwo.hide();
     $stepThree.hide();
+
+    $inputNome.keyup(function(){
+        const closest = $(this).closest('.input-data');
+        if(!this.value || this.value.trim().length < 2){
+            return closest.addClass('error');
+        }
+        return closest.removeClass('error');
+    });
+
+    $inputSobrenome.keyup(function(){
+        const closest = $(this).closest('.input-data');
+        if(!this.value || this.value.trim().length <2){
+            return closest.addClass('error')
+        }
+        return closest.removeClass('error');
+    });
+
+    $inputDataNascimento.on('focus', function(){
+        this.type = 'date';
+    });
+
+    $inputDataNascimento.on('blur', function(){
+        if(!this.value){
+            this.type = 'text';
+        }
+    })
+
+        
+
 }
+   
 
 init();
