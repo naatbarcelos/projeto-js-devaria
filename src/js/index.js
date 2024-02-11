0a21e5efac1d37b1e165e9dfa9587c180fc39efe
naatbarcelos <naathalia.b@hotmail.com>
2024-02-11 19:59:14 -0300

    Bug resolvido

--- src/js/index.js
+++ src/js/index.js
@@ -4,30 +4,104 @@ const $stepOne = $('.step.one');
 const $stepTwo = $('.step.two');
 const $stepThree = $('.step.three');
 
+const $containerBtnFormOne = $('#containerBtnFormOne');
+const $btnFormOne = $('#btnFormOne');
 const $inputNome = $('#nome');
 const $inputSobrenome = $('#sobrenome');
+const $inputDataNascimento = $('#dataNascimento');
+const $inputEmail = $('#email');
+const $inputMinibio = $('#minibio');
+
+
+let nomeValido = false;
+let sobrenomeValido = false;
+let dataNascimentoValido = false;
+let emailValido = false;
 
 function init(){
     $stepText.text('Passo 1 de 3 - Dados pessoais');
     $stepDescription.text('Descreva seus dados para que possamos te conhecer melhor.');
     $stepTwo.hide();
     $stepThree.hide();
+
+    const minLengthText = 2;
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+
+    function validarInput(element, minLength, maxLength, regex) {
+    const closest = $(element).closest('.input-data');
+        if(!element.value 
+            || (minLength && element.value.trim().length < minLength)
+            || (maxLength && element.value.trim().length > maxLength)
+            || (regex && !element.value.toLowerCase().match(regex))
+            
+            ) {
+            closest.addClass('error');
+            return false;
+        }
+        closest.removeClass('error');
+        return true;
 }
 
-$inputNome.keyup(function(){
-    const closest = $(this).closest('.input-data');
-    if(!this.value || this.value.trim().lenght < 2){
-        return closest.addClass('error');
-    } 
-    return closest.removeClass('error');
-}); 
-
- $inputSobrenome.keyup(function(){
-    const closest = $(this).closest('.input-data')
-    if(!this.value || this.value.trim().lenght <2){
-        return closest.addClass('error');
+function validaFormularioUm (){
+    if(nomeValido && sobrenomeValido && emailValido && dataNascimentoValido){
+        $containerBtnFormOne.removeClass('disabled');
+        $btnFormOne.removeClass('disabled');
+        $btnFormOne.on('click', iniciarFormulario2);
+    }else{
+        $containerBtnFormOne.addClass('disabled');
+        $btnFormOne.addClass('disabled');
     }
-    return closest.removeClass('error');
- })
+}
+
+function iniciarFormulario2(){
+    
+}
+
+    $inputNome.keyup(function(){
+       nomeValido = validarInput(this, minLengthText);
+       validaFormularioUm();
+    });
+
+    $inputSobrenome.keyup(function(){
+       sobrenomeValido = validarInput(this, minLengthText);
+       validaFormularioUm();
+    });
+
+    $inputDataNascimento.keyup(function (){
+       dataNascimentoValido = validarInput(this, minLengthText);
+       validaFormularioUm();
+    })
+    
+    $inputDataNascimento.change(function (){
+        dataNascimentoValido = validarInput(this, minLengthText);
+        validaFormularioUm();
+    })
+    
+    
+
+    $inputEmail.keyup(function (){
+       emailValido = validarInput(this, null, null, emailRegex);
+       validaFormularioUm();
+    })
+
+    $inputMinibio.keyup(function (){
+        validaFormularioUm();
+     })
+
+ 
+     $inputDataNascimento.on('focus', function () {
+        this.type = 'date';
+    });
+
+    $inputDataNascimento.on('blur', function () {
+        if(!this.value){
+            this.type = 'text';
+        }
+    })
+        
+
+}
+   
 
 init();
